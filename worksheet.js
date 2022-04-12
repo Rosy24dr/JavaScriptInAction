c610a692faff6d9eb730ad2a485c5055d76b711e
rossebell <Rosy24dr@outlook.com>
2022-04-12 12:25:22 -0400

    Commented out previous problems and finish problem 3 regarding the Meals.

--- worksheet.js
+++ worksheet.js
@@ -1,12 +1,27 @@
 //Days of the week problem
-let dayOfWeek = 'Monday'
-console.log(dayOfWeek)
-
-dayOfWeek = 'Friday'
-console.log(`I can not wait for ${dayOfWeek}!`)
+// let dayOfWeek = 'Monday'
+// console.log(dayOfWeek)
+// dayOfWeek = 'Friday'
+// console.log(`I can not wait for ${dayOfWeek}!`)
 
 //Users input problem
-let animalInput = prompt('What is your favorite animal?')
-let colorInput = prompt('What is your favorite color?')
+//let animalInput = prompt('What is your favorite animal?')
+//let colorInput = prompt('What is your favorite color?')
+//console.log(`I have never seen a ${colorInput} ${animalInput}!`)
 
-console.log(`I have never seen a ${colorInput} ${animalInput}!`)
+
+//Meal problem
+// let timeOfDay = 1300
+// let favoriteMeal = ''
+
+// if (timeOfDay < 1200){
+//     alert('Mangu con salami y huevo frito!');
+// }
+// else if (timeOfDay > 1200 && timeOfDay < 1700){
+//     alert('Fritos con carne frita')
+// }
+// else if (timeOfDay > 1700){
+//     alert('Sancocho')
+// }
+
+// console.log(timeOfDay)
