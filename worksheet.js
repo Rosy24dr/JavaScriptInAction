7aca6113337d81f856a8416db34736240799a62b
rossebell <Rosy24dr@outlook.com>
2022-04-12 11:54:10 -0400

    Added assignment to the ReadMe file, Added comments into the JS file, finishd problem two of user input

--- worksheet.js
+++ worksheet.js
@@ -1,5 +1,12 @@
+//Days of the week problem
 let dayOfWeek = 'Monday'
 console.log(dayOfWeek)
 
 dayOfWeek = 'Friday'
 console.log(`I can not wait for ${dayOfWeek}!`)
+
+//Users input problem
+let animalInput = prompt('What is your favorite animal?')
+let colorInput = prompt('What is your favorite color?')
+
+console.log(`I have never seen a ${colorInput} ${animalInput}!`)
